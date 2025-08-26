6045df1c36590d6423982e283589eb4595dac5cc
NEPotato <james.boydens@gmail.com>
2025-08-26 09:17:17 +0200

    api_content

---
+++ main.js
@@ -0,0 +1,14 @@
+const APIurl = "https://ingrwf12.cepegra-frontend.xyz/cockpit1/api/content/item/voyages";
+
+fetch(APIurl)
+.then(res => res.json())
+.then(data => {
+  const voyage = document.querySelector(".voyage");
+  console.log(data);
+  voyage.innerHTML = data["voyages-label"];
+  const description = document.querySelector(".description");
+  description.innerHTML = data["voyages-description"];
+  const prix = document.querySelector(".prix");
+  prix.innerHTML = data["voyages-prix"];
+  //data.voyages-prix ne fonctionne pas car le - n'est pas pris en compte
+})
