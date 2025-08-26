0e4a61d582576a0f08023edbed8541c244dff570
NEPotato <james.boydens@gmail.com>
2025-08-26 15:50:46 +0200

    notification

---
+++ notification.js
@@ -0,0 +1,31 @@
+const notification = () => {
+  const btn = document.querySelector(".notification_btn");
+  btn.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    let myNotification = null;
+    const options = {
+      body: "Ceci est une notification",
+      icon: "icons/favicon-256x256.png",
+      vibrate: [100, 50, 100],
+      tag: "notification",
+    };
+    if (!("Notification") in window) {
+      alert("Votre navigateur ne supporte pas les notifications");
+    } else if (Notification.permission === "granted") {
+      console.log("notification granted");
+      myNotification = new Notification("Hello", options);
+    } else {//si nous n'avons pas les droits, on va les demander
+      Notification.requestPermission().then((result) => {
+        if (result === "denied") {
+          console.log("notification denied");
+        } else if (result === "granted") {
+          console.log("notification granted");
+          myNotification = new Notification("Hello", options);
+        }
+      });
+      
+    }
+});
+}
+
+notification()
