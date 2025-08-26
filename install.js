276bff1d54649dcf97a6db1ef0001201aa6dd104
NEPotato <james.boydens@gmail.com>
2025-08-26 11:29:25 +0200

    install_prompt

---
+++ install.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".install_btn");
+let deferredPrompt = null;
+
+btn.classList.add("hidden");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  //cet event ne se lancera que si le navigateur supporte les PWA
+  e.preventDefault();
+  console.log("beforeinstallprompt fired");
+  deferredPrompt = e; // Store the event to be able to trigger the prompt
+  btn.classList.remove("hidden");
+});
+
+btn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  deferredPrompt.prompt();
+  const result = await deferredPrompt.userChoice;
+  if (result.outcome === "accepted") btn.classList.add("hidden");
+  deferredPrompt = null;//reset
+});
+
+window.addEventListener("appinstalled", (e) => {
+  console.log("appinstalled fired");
+});
